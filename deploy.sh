1049e4f295c0e2f6bb9f100a717517bd3f68b33b
mqjd <1378415278@qq.com>
2020-04-24 09:59:33 +0800

    blog

--- deploy.sh
+++ deploy.sh
@@ -10,33 +10,21 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'b.xugaoyi.com' > CNAME
+
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:mqjd/bigdata-notes.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://mqjd:${GITHUB_TOKEN}@github.com/mqjd/bigdata-notes.git
+  git config --global user.name "mqjd"
+  git config --global user.email "1378415278@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master:blog-pages # 推送到github
 
-# deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  # codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
-  codingUrl=git@e.coding.net:xgy/xgy.git
-else
-  # codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
 
 
 cd - # 退回开始所在目录
