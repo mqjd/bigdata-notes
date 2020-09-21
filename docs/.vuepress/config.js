df372d78047c51a81ab90fdf67a40b096c89e865
mqjd <1378415278@qq.com>
2020-09-21 22:04:29 +0800

    log

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -95,18 +95,31 @@ module.exports = {
       // ]
     }],
     [require('./plugins/drawio-viewer'), {}],
-    // [require('./plugins/enhanced-search'), { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）,增加后导致drawio viewer不可用
-    //   thirdparty: [ // 可选，默认 []
-    //     {
-    //       title: '在百度中搜索',
-    //       frontUrl: 'https://www.baidu.com/s?wd=',
-    //     },
-    //     {
-    //       title: '在Google中搜索',
-    //       frontUrl: 'https://www.google.com/search?q='
-    //     }
-    //   ]
-    // }],
+    ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+      thirdparty: [ // 可选，默认 []
+        {
+          title: '在MDN中搜索',
+          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+          behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+        },
+        {
+          title: '在Runoob中搜索',
+          frontUrl: 'https://www.runoob.com/?s=',
+        },
+        {
+          title: '在Vue API中搜索',
+          frontUrl: 'https://cn.vuejs.org/v2/api/#',
+        },
+        {
+          title: '在Bing中搜索',
+          frontUrl: 'https://cn.bing.com/search?q='
+        },
+        {
+          title: '通过百度搜索本站的',
+          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20'
+        }
+      ]
+    }],
     'vuepress-plugin-baidu-autopush', // 百度自动推送
     ['one-click-copy', { // 代码块复制按钮
       copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
